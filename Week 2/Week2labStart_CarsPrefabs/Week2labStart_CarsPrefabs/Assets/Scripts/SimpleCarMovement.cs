1d3b29bca2f81a077e0aa46d55bb3c34ebe349e3
alfonsusac <alfonsusac@gmail.com>
2022-07-04 12:28:40 +0200

    Week 2

---
+++ Week 2/Week2labStart_CarsPrefabs/Week2labStart_CarsPrefabs/Assets/Scripts/SimpleCarMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCarMovement : MonoBehaviour
+{
+	public float speed=1;	
+
+	Rigidbody rb;
+
+    void Start()
+    {
+		rb=GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+		Vector3 moveVector = transform.forward;
+
+		rb.velocity = moveVector * speed;
+    }
+}
---
+++ Week 2/Week2labStart_CarsPrefabs/Week2labStart_CarsPrefabs/Assets/Scripts/SimplePlayerControls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerControls : MonoBehaviour
+{
+	public float speed=1;
+
+    Rigidbody rb;
+    Vector3 startPosition;
+
+    void Start()
+    {
+		rb=GetComponent<Rigidbody>();
+		startPosition = transform.position;
+    }
+
+    void Update()
+    {
+		Vector3 moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+		transform.Translate(moveVector * speed);
+    }
+
+    void ResetPosition() {
+        if (rb != null) {
+            rb.position = startPosition;
+        } else {
+            Debug.Log("Warning: no rigidbody attached to player");
+        }
+    }
+}
