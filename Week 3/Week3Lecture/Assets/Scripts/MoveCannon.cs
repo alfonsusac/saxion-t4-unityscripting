a3f00c3e1d1756c16a74061d88f89eee6d864686
alfonsusac <alfonsusac@gmail.com>
2022-07-06 12:38:12 +0200

    Export Rpoject tried on another project

---
+++ Week 3/Week3Lecture/Assets/Scripts/MoveCannon.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Smoothing { None, Lerp, Slerp, MoveTowards}
+
+public class MoveCannon : MonoBehaviour
+{
+    public Smoothing SmoothingMethod;
+    public GameObject CannonHeadPart;
+    public string NameOfPlayerObject = "Player";
+    public static GameObject Player;
+
+    public float CannonRotationMaxSpeed = 1f;
+    public float CannonHeadRotationMaxSpeed = 1f;
+
+    private bool playerSeen = false;
+    public bool PlayerSeen { get { return playerSeen;  } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (CannonHeadPart == null) 
+            CannonHeadPart = transform.Find("CannonHeadPart").gameObject;
+        if (Player == null)
+            Player = GameObject.Find(NameOfPlayerObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePositionToPlayer = GetRelativePosition();
+        (GameObject objectHit, bool isPlayerVisible) = IsPlayerVisible(relativePositionToPlayer);
+        
+        if (isPlayerVisible && objectHit.name == "Player")
+        {
+            playerSeen = true;
+            RotateCannon(relativePositionToPlayer);
+        }
+        else
+        {
+            playerSeen = false;
+        }
+
+
+
+    }
+
+    (GameObject, bool) IsPlayerVisible(Vector3 relativePosition)
+    {
+        RaycastHit hitInfo;
+        Debug.DrawRay(CannonHeadPart.transform.position, relativePosition);
+        if (Physics.Raycast(CannonHeadPart.transform.position, relativePosition, out hitInfo, Mathf.Infinity))
+            return (hitInfo.transform.gameObject, true);
+        else
+            return (null, false);
+    }
+    Vector3 GetRelativePosition()
+    {
+        return Player.transform.position - CannonHeadPart.transform.position;
+    }
+    void RotateCannon(Vector3 relativePosition)
+    {
+        // Gets the relative displacement from player to cannon
+        Vector3 relativePos = Player.transform.position - CannonHeadPart.transform.position;
+
+        // Rotate Base of Turret left to right
+        Vector3 relativePosXZPlane = new Vector3(relativePos.x, 0, relativePos.z);
+        Quaternion rotationToLookAt = Quaternion.LookRotation(relativePosXZPlane);
+        InstantRotation(transform, rotationToLookAt);
+
+        // Rotate Head of turrent up and down
+        Vector3 eulerRotation = Quaternion.LookRotation(relativePos).eulerAngles;
+        Quaternion newRotation = Quaternion.Euler(eulerRotation.x, 0, 90);
+        InstantRotation(CannonHeadPart.transform, newRotation, localRotation: true);
+    }
+
+    void InstantRotation(Transform obj, Quaternion rotateTo, bool localRotation = false)
+    {
+        Quaternion rotationStep = new Quaternion();
+        // Instant
+        if (SmoothingMethod == Smoothing.None)
+            rotationStep = rotateTo;
+
+        // Eased
+        else if (SmoothingMethod == Smoothing.Lerp)
+            rotationStep = Quaternion.Lerp(
+                a: localRotation ? obj.localRotation : obj.rotation,
+                b: rotateTo,
+                t: Time.deltaTime * CannonRotationMaxSpeed
+            );
+
+        //Slerp
+        else if (SmoothingMethod == Smoothing.Slerp)
+            rotationStep = Quaternion.Slerp(
+                a: localRotation ? obj.localRotation : obj.rotation,
+                b: rotateTo,
+                t: Time.deltaTime * CannonRotationMaxSpeed
+            );
+
+        //Constant Movement
+        else if (SmoothingMethod == Smoothing.MoveTowards)
+            rotationStep = Quaternion.RotateTowards(
+                           from: localRotation ? obj.localRotation : obj.rotation,
+                             to: rotateTo,
+                maxDegreesDelta: 1f
+            );
+
+        if (localRotation) obj.localRotation = rotationStep;
+        else obj.rotation = rotationStep;
+    }
+}
---
+++ Week 3/Week3Lecture/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Control { Input, Random}
+
+public class MovePlayer : MonoBehaviour
+{
+	public Control control;
+	public float moveSpeed;
+	public float turnSpeed;
+
+	Rigidbody rb;
+
+	float move;
+	float turn;
+	float angleChange;
+
+	bool grounded;
+
+    void Start()
+    {
+		rb=GetComponent<Rigidbody>();
+    }
+
+	public void Update() {
+		if (control==Control.Input) {
+			KeyControls();
+		} else {
+			AutoControls();
+		}
+	}
+
+	void KeyControls() {
+		move = Input.GetAxis("Vertical");
+		turn = Input.GetAxis("Horizontal");
+	}
+
+	void AutoControls() {
+		if (angleChange!=0) {
+			turn = Mathf.Sign(angleChange);
+		} 
+		move=1;
+	}
+
+	void FixedUpdate()
+    {
+		if (CheckGrounded()) {
+			if (turn!=0) {
+				float oldAngleChange = angleChange;
+				float frameTurn = turn * turnSpeed * Time.fixedDeltaTime;
+				angleChange-=frameTurn;
+				if (angleChange * oldAngleChange<0) {
+					angleChange=0;
+					turn=0;
+				}
+
+				transform.Rotate(0, frameTurn, 0);
+			}
+			rb.velocity = transform.forward * moveSpeed * move + new Vector3(0,rb.velocity.y,0);
+		} 
+    }
+
+	bool CheckGrounded() {
+		return Physics.Raycast(transform.position, Vector3.down, 1.1f);
+	}
+
+	private void OnCollisionStay(Collision collision) {
+		// When colliding with a wall, choose a random turn angle, unless an angle was already chosen:
+		if (angleChange==0 && collision.GetContact(0).normal.y<0.7f) {
+			angleChange = Random.value * 360 - 180;
+		}
+	}
+}
