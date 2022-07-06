ca16dcf00ec6ad93dfec5373fb85487ce9898a8b
alfonsusac <alfonsusac@gmail.com>
2022-07-06 10:23:25 +0200

    7

--- Week 4/Week4labStart/Assets/Scripts/MoveCannon.cs
+++ Week 4/Week4labStart/Assets/Scripts/MoveCannon.cs
@@ -2,29 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Smoothing { None, Lerp, Slerp, MoveTowards}
+
 public class MoveCannon : MonoBehaviour
 {
+    public Smoothing SmoothingMethod;
     public GameObject CannonHeadPart;
     public GameObject Player;
+
     public float CannonRotationMaxSpeed = 1f;
     public float CannonHeadRotationMaxSpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(transform.GetChild(0).name);
         if (CannonHeadPart == null) CannonHeadPart = transform.Find("CannonHeadPart").gameObject;
-        if (Player == null) Player = transform.parent.Find("Player").gameObject;
+        if (Player == null)         Player = transform.parent.Find("Player").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RotateCannon();
+        Vector3 relativePositionToPlayer = GetRelativePosition();
+        (GameObject objectHit, bool isPlayerVisible) = IsPlayerVisible(relativePositionToPlayer);
+        if (isPlayerVisible && objectHit.name == "Player")
+        {
+            RotateCannon(relativePositionToPlayer);
+        }
     }
 
-    public bool EasedMotion = true;
-    void RotateCannon()
+    (GameObject, bool) IsPlayerVisible(Vector3 relativePosition)
+    {
+        RaycastHit hitInfo;
+        Debug.DrawRay(CannonHeadPart.transform.position, relativePosition);
+        if (Physics.Raycast(CannonHeadPart.transform.position, relativePosition, out hitInfo, Mathf.Infinity))
+            return (hitInfo.transform.gameObject, true);
+        else
+            return (null, false);
+    }
+    Vector3 GetRelativePosition()
+    {
+        return Player.transform.position - CannonHeadPart.transform.position;
+    }
+    void RotateCannon(Vector3 relativePosition)
     {
         // Gets the relative displacement from player to cannon
         Vector3 relativePos = Player.transform.position - CannonHeadPart.transform.position;
@@ -32,38 +52,46 @@ public class MoveCannon : MonoBehaviour
         // Rotate Base of Turret left to right
         Vector3 relativePosXZPlane = new Vector3(relativePos.x, 0, relativePos.z);
         Quaternion rotationToLookAt = Quaternion.LookRotation(relativePosXZPlane);
-        
-        // Instantly 
-        if(!EasedMotion)
-            transform.rotation = rotationToLookAt;
-        
+        InstantRotation(transform, rotationToLookAt);
+
+        // Rotate Head of turrent up and down
+        Vector3 eulerRotation = Quaternion.LookRotation(relativePos).eulerAngles;
+        Quaternion newRotation = Quaternion.Euler(eulerRotation.x, 0, 90);
+        InstantRotation(CannonHeadPart.transform, newRotation, localRotation: true);
+    }
+
+    void InstantRotation(Transform obj, Quaternion rotateTo, bool localRotation = false)
+    {
+        Quaternion rotationStep = new Quaternion();
+        // Instant
+        if (SmoothingMethod == Smoothing.None)
+            rotationStep = rotateTo;
+
         // Eased
-        else
-            transform.rotation = Quaternion.Lerp(
-                a: transform.rotation, 
-                b: rotationToLookAt, 
+        else if (SmoothingMethod == Smoothing.Lerp)
+            rotationStep = Quaternion.Lerp(
+                a: localRotation ? obj.localRotation : obj.rotation,
+                b: rotateTo,
                 t: Time.deltaTime * CannonRotationMaxSpeed
             );
 
-        // Rotate Head of turrent up and down
-        Vector3 rotationToLookAt2 = Quaternion.LookRotation(relativePos).eulerAngles;
-
-        // Instantly 
-        if(!EasedMotion)
-            CannonHeadPart.transform.localRotation = Quaternion.Euler(
-                        x: rotationToLookAt2.x,
-                        y: 0,
-                        z: 90);
-
-        // Eased
-        else
-            CannonHeadPart.transform.localRotation = Quaternion.Lerp(
-                    a: CannonHeadPart.transform.localRotation,
-                    b: Quaternion.Euler(
-                        x: rotationToLookAt2.x,
-                        y: 0,
-                        z: 90),
-                    t: Time.deltaTime * CannonHeadRotationMaxSpeed
+        //Slerp
+        else if (SmoothingMethod == Smoothing.Slerp)
+            rotationStep = Quaternion.Slerp(
+                a: localRotation ? obj.localRotation : obj.rotation,
+                b: rotateTo,
+                t: Time.deltaTime * CannonRotationMaxSpeed
             );
+
+        //Constant Movement
+        else if (SmoothingMethod == Smoothing.MoveTowards)
+            rotationStep = Quaternion.RotateTowards(
+                           from: localRotation ? obj.localRotation : obj.rotation,
+                             to: rotateTo,
+                maxDegreesDelta: 1f
+            );
+
+        if (localRotation) obj.localRotation = rotationStep;
+        else obj.rotation = rotationStep;
     }
 }
